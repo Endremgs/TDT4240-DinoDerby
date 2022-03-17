8549dd1ed237b739b5d9adf300acd052eb811447
endremgs <endremg@stud.ntnu.no>
2022-03-17 16:01:56 +0100

    wip: rm ios & html

--- DinoDerby/desktop/src/com/mygdx/game/DesktopInterfaceClass.java
+++ DinoDerby/desktop/src/com/mygdx/game/DesktopInterfaceClass.java
@@ -1,21 +1,21 @@
 package com.mygdx.game;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import static android.content.ContentValues.TAG;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+//import static android.content.ContentValues.TAG;
 
 public class DesktopInterfaceClass implements FireBaseInterface {
 
-    FirebaseDatabase database;
-    DatabaseReference myRef;
+//    FirebaseDatabase database;
+//    DatabaseReference myRef;
     public DesktopInterfaceClass() {
-        // Write a message to the database
-        this.database = FirebaseDatabase.getInstance();
-        this.myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
+//        // Write a message to the database
+//        this.database = FirebaseDatabase.getInstance();
+//        this.myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello, World!");
     }
 
     @Override
@@ -25,9 +25,9 @@ public class DesktopInterfaceClass implements FireBaseInterface {
 
     @Override
     public void firstFireBaseText() {
-        if (myRef != null) {
-            myRef.setValue("hello world!");
-        }
+//        if (myRef != null) {
+//            myRef.setValue("hello world!");
+//        }
     }
 
 }
