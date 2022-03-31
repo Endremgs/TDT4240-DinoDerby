800f8981ddb673a6eb7d6e67833f4969e9ea55a1
thomaset <thomaset@stud.ntnu.no>
2022-03-31 11:43:31 +0200

    #5 lagde initial SettingsScreen lagde GameSettings for å kontrolere settings.

---
+++ DinoDerby/core/src/com/mygdx/game/GameSettings.java
@@ -0,0 +1,55 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
+public class GameSettings {
+
+    //https://libgdx.badlogicgames.com/ci/nightlies/docs/api/com/badlogic/gdx/Preferences.html
+    private static final String MUSIC_VOLUME = "volume";
+    private static final String MUSIC_ENABLED = "music.enabled";
+    private static final String SOUND_VOLUME = "sound";
+    private static final String SOUND_ENABLED = "sound.enabled";
+    private static final String PREFERENCES_NAME = "gameSett";
+
+    public Preferences getPrefs(){
+        return Gdx.app.getPreferences(PREFERENCES_NAME);
+    }
+
+    public void setMusicEnabled(boolean musicEnabled){
+        getPrefs().putBoolean(MUSIC_ENABLED, musicEnabled);
+        getPrefs().flush();
+    }
+
+    public boolean isMusicEnabled(){
+        return getPrefs().getBoolean(MUSIC_ENABLED, true);
+    }
+
+    public float getMusicVolume(){
+        return getPrefs().getFloat(MUSIC_VOLUME, 1f);
+    }
+
+    public void setMusicVolume(float musicVolume){
+        getPrefs().putFloat(MUSIC_VOLUME,musicVolume);
+        getPrefs().flush();
+    }
+
+    public void setSoundEnabled(boolean soundEnabled){
+        getPrefs().putBoolean(SOUND_ENABLED, soundEnabled);
+        getPrefs().flush();
+    }
+
+    public boolean isSoundEnabled(){
+        return getPrefs().getBoolean(SOUND_ENABLED, true);
+    }
+
+    public void setSoundVolume(float soundVolume){
+        getPrefs().putFloat(SOUND_VOLUME, soundVolume);
+        getPrefs().flush();
+    }
+
+    public float getSoundVolume(){
+        return getPrefs().getFloat(SOUND_VOLUME, 1f);
+    }
+
+}
