1442f85752c9d0f77346d86cd653e86000876d6f
andrhae <andrhae@stud.ntnu.no>
2022-04-25 14:04:26 +0200

    add(#65): ghost system

---
+++ DinoDerby/core/src/com/mygdx/game/entity/systems/GhostSystem.java
@@ -0,0 +1,36 @@
+package com.mygdx.game.entity.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.entity.components.GhostComponent;
+import com.mygdx.game.entity.components.TransformComponent;
+
+import java.util.Map;
+
+public class GhostSystem extends IteratingSystem {
+
+    private MyGdxGame game;
+
+    private ComponentMapper<GhostComponent> cmGhost;
+    private ComponentMapper<TransformComponent> cmTransform;
+
+    public GhostSystem(MyGdxGame game) {
+        super(Family.all(GhostComponent.class).get());
+
+        cmGhost = ComponentMapper.getFor(GhostComponent.class);
+        cmTransform = ComponentMapper.getFor(TransformComponent.class);
+        this.game = game;
+    }
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        GhostComponent ghost = cmGhost.get(entity);
+        TransformComponent transform = cmTransform.get(entity);
+        Map<String, Float> playerMap = game.getPlayers().get(ghost.playerID);
+        transform.position.x = playerMap.get("xPos");
+        transform.position.y = playerMap.get("yPos");
+
+    }
+}
